2ebfb9245bae250295dfd5ed0ff0c82a18b0e951
cjmateos <mateos83@gmail.com>
2016-12-02 11:49:55 +0100

    Export JAVA_HOME

--- resources/redborder-common.sh
+++ resources/redborder-common.sh
@@ -4,6 +4,7 @@ export RBLIB="${RBDIR}/lib"
 export RBETC="/etc/redborder"
 export PATH=$PATH:$RBBIN
 export EDITOR=vim
+export JAVA_HOME=/usr/lib/jvm/jre
 alias bwm-ng='bwm-ng -u bits -t 1000 -d'
 alias log='tail -n 200 -f'
 # Banner
