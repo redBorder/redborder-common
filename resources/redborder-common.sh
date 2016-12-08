6386d9189f2bbd661668384f736ac2ddc08125f3
Juan J. Prieto <jjptapia@gmail.com>
2016-12-08 09:18:53 +0100

    add history to syslog activity for bash

--- resources/redborder-common.sh
+++ resources/redborder-common.sh
@@ -7,6 +7,19 @@ export EDITOR=vim
 export JAVA_HOME=/usr/lib/jvm/jre
 alias bwm-ng='bwm-ng -u bits -t 1000 -d'
 alias log='tail -n 200 -f'
+
+# Send History to syslog
+PROMPT_COMMAND=$(history -a)
+typeset -r PROMPT_COMMAND
+function log2syslog
+{
+	declare command
+	command=$BASH_COMMAND
+	logger -p local1.notice -t bash -i -- $USER : $PWD : $command
+}
+trap log2syslog DEBUG
+
 # Banner
 figlet -f slant "redborder-ng"
 echo ""
+
