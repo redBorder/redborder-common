4a1097a9207284849d81948b9aea13049bd90c5a
ljblancoredborder <ljblanco@redborder.com>
2026-07-02 16:20:22 +0100

    update copyright, bump version

--- resources/bin/rb_vmware_exsi_monitor.sh
+++ resources/bin/rb_vmware_exsi_monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #######################################################################
-# Copyright (c) 2014 ENEO Tecnología S.L.
+# Copyright (c) 2026 ENEO Tecnología S.L.
 # This file is part of redBorder.
 # redBorder is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License License as published by
--- resources/scripts/rb_vmware_exsi_monitor.rb
+++ resources/scripts/rb_vmware_exsi_monitor.rb
@@ -1,8 +1,19 @@
 #!/usr/bin/env ruby
-#
-# Copyright (c) 2026 ENEO Tecnologia S.L.
-# All rights reserved.
-#
+
+########################################################################
+# Copyright (c) 2026 ENEO Tecnología S.L.
+# This file is part of redBorder.
+# redBorder is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# redBorder is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License License for more details.
+# You should have received a copy of the GNU Affero General Public License License
+# along with redBorder. If not, see <http://www.gnu.org/licenses/>.
+# ######################################################################
 
 require 'optparse'
 require 'json'
